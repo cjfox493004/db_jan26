2f2b791cdda258f3b610eb6330f78a0d999f7f8d
cjfox493004 <cjfox493@gmail.com>
2026-02-18 15:46:13 +0000

    ouch

--- Feb16/app.py
+++ Feb16/app.py
@@ -5,23 +5,31 @@ import gradio as gr
 conn = sqlite3.connect('../baseball.db')
 cursor = conn.cursor()
 query = """
-WITH top_hitters as (SELECT nameFirst, nameLast FROM batting inner join people
+WITH top_hitters as (SELECT nameFirst, nameLast, batting.playerID FROM batting inner join people
 on batting.playerID = people.playerID
 Where teamID = "PHI"
 Group by batting.playerID
 Order by SUM(HR) DESC
 LIMIT 10)
 
-SELECT CONCAT (nameFirst, ' ', nameLast) as Player FROM top_hitters
+SELECT CONCAT (nameFirst, ' ', nameLast) as Player, playerID FROM top_hitters
 ORDER by nameLast
 """
 cursor.execute(query)
 records = cursor.fetchall()
 conn.close()
 
-players = []
-for record in records:
-    players.append(record[0])
+#players = []
+#for record in records:
+ #   players.append(record[0])
 
-print(players)
 
+with gr.Blocks() as iface:
+    gr.Dropdown(records, interactive=True)
+    gr.LinePlot(records, x = "Year", y = "Homeruns")
+
+iface.launch()
+
+
+
+print(records)
